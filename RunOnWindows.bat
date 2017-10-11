e9e01ae8ee512acdb4fb3bf05ca4b9aa8ccbda99
huntbachdj <huntbachdj@familysearch.org>
2017-10-10 23:09:33 -0600

    Changed path to JAR in batch file.

--- RunOnWindows.bat
+++ RunOnWindows.bat
@@ -1,6 +1,6 @@
 @echo off
 REM *************************************************************************
-REM * Script to help run githubreports on Windows from the githubreports "target" directory.
+REM * Script to help run githubreports on Windows from the "githubreports" directory.
 REM *
 REM * Don't forget to create the "oitskilleval" S3 bucket before running.
 REM *
@@ -10,4 +10,4 @@ set AWS_ACCESS_KEY_ID=<your AWS access key id>
 set AWS_SECRET_ACCESS_KEY=<your AWS secret access key>
 set AWS_REGION=<us-east-1|us-west-2|eu-west-1>
 
-java -jar githubreports-1.0.0-SNAPSHOT.jar -l<GitHubLogin> -o<GitHubOrganization> -p<GitHubPassword or Personal Access Token>
+java -jar .\target\githubreports-1.0.0-SNAPSHOT.jar -l<GitHubLogin> -o<GitHubOrganization> -p<GitHubPassword or Personal Access Token>
